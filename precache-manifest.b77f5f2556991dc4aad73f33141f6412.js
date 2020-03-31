2ad68790a6b7ee33b4ed878d07a96cced6da93c8
Stan Keathly <skeathly@gmail.com>
2020-03-31 15:26:14 -0500

    Updates

---
+++ precache-manifest.b77f5f2556991dc4aad73f33141f6412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79adb550a35fd6326b0a0fa46b7ac619",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de7d379af360b43fa986",
+    "url": "/monsters-rolodex/static/css/main.76f91320.chunk.css"
+  },
+  {
+    "revision": "6e476ed961cdc15311f6",
+    "url": "/monsters-rolodex/static/js/2.f568786c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f568786c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7d379af360b43fa986",
+    "url": "/monsters-rolodex/static/js/main.d0b3c35d.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
